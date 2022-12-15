4b49b9beb5be186cb4fc1042ab5dd1ebe876a876
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-12-15 19:41:16 +0000

    added functionality to cycle through collection of images clicking button - created a component ImageCollection

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ActivityIndicator, Image, Button, Alert } from 'react-native';
 
@@ -8,18 +8,45 @@ const IMAGES = ['https://images.pexels.com/photos/837745/pexels-photo-837745.jpe
   'https://images.pexels.com/photos/725255/pexels-photo-725255.jpeg',
   'https://images.pexels.com/photos/209943/pexels-photo-209943.jpeg']
 
+let currentImgIdx = 0;
+
+
+// creating component which shows an image from a collection of images, props = properties for the component
+// button allows user to cycle through collection of images
+const ImageCollection = (props) => {
+  // Declare a new state variable, which we'll call "currentImgIdx"
+  const [currentImgIdx, setCurrentImgIdx] = useState(0) //  [<getter>,<setter>]  for state variable
+                                                        // useState sets initial value
+  return (
+    <View>
+      <Image
+        style={{ width: 350, height: 300 }}
+        source={{ uri: IMAGES[currentImgIdx] }}    // an image from the IMAGES array is displayed
+      />
+      <Button
+        onPress={() => {
+          if (currentImgIdx < IMAGES.length - 1) {  // if image isnt the last of the collection
+            setCurrentImgIdx(currentImgIdx + 1)
+          } else {                                  // image is the last, set img to the first one
+            setCurrentImgIdx(0)
+          }
+        }}
+        title="Next Image"
+        accessibilityLabel="Show the next image"
+      />
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Awesome App</Text>
-      <Image
-        style={{ width: 350, height: 300 }}
-        source={{ uri: IMAGES[0] }}
-      />
       <Button
         title="Press me for alert"
         onPress={() => Alert.alert('Simple Button pressed')}
       />
+      <ImageCollection />
       <StatusBar style="auto" />
     </View>
   );
